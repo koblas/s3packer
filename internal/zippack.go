f091324e3e8295f714480d0ee55e073c8db2cbf9
David Koblas <david@koblas.com>
2020-12-16 10:01:11 -0500

    Better streaming abstraction

--- internal/zippack.go
+++ internal/zippack.go
@@ -12,12 +12,12 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
-type NameQueue struct {
+type nameQueue struct {
 	bucket string
 	key    string
 }
 
-type DataQueue struct {
+type dataQueue struct {
 	name string
 	data []byte
 }
@@ -30,25 +30,26 @@ func ZipPack(dest string, source []string) error {
 	}
 
 	// FileGeneration -> Downloader -> Zip Adder -> Writer
+	nameQueue := generateFiles(source)
+	zipQueue := downloadFiles(nameQueue)
+	reader := addToZip(zipQueue)
 
-	pipeReader, pipeWriter := io.Pipe()
-	defer pipeReader.Close()
-	zipper := NewZipper(pipeWriter)
-
+	defer reader.Close()
 	wg := sync.WaitGroup{}
-	wg.Add(2)
 
 	if destFile.Scheme == "s3" {
+		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			if err := uploadToS3(*destFile, pipeReader); err != nil {
+			if err := uploadToS3(*destFile, reader); err != nil {
 				log.Fatal(err)
 			}
 		}()
 	} else if destFile.Scheme == "file" {
+		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			if err := uploadToLocal(*destFile, pipeReader); err != nil {
+			if err := uploadToLocal(*destFile, reader); err != nil {
 				log.Fatal(err)
 			}
 		}()
@@ -56,27 +57,6 @@ func ZipPack(dest string, source []string) error {
 		fmt.Println("ERROR unknown file destination")
 		os.Exit(1)
 	}
-
-	nameQueue := make(chan NameQueue)
-	zipQueue := make(chan DataQueue)
-
-	go func() {
-		defer wg.Done()
-		if err := generateFiles(nameQueue, source); err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	go downloadFiles(nameQueue, zipQueue)
-	go func() {
-		// addToZip is a channel pipe, it doesn't know that zipper doesn't
-		// close the writer pipe when it's closed.  Closing the pipe causes
-		// the uploader to know that it's done with the input
-		defer pipeWriter.Close()
-		if err := addToZip(zipQueue, zipper); err != nil {
-			log.Fatal(err)
-		}
-	}()
 
 	wg.Wait()
 
@@ -123,87 +103,104 @@ func uploadToLocal(destFile FileURI, reader io.Reader) error {
 	return nil
 }
 
-func generateFiles(queue chan NameQueue, source []string) error {
-	defer close(queue)
+func generateFiles(source []string) chan nameQueue {
+	queue := make(chan nameQueue)
 
-	svc := SessionNew()
-	seen := map[string]bool{}
+	go func() {
+		defer close(queue)
 
-	for _, sourceFile := range source {
-		for todo := []string{sourceFile}; len(todo) != 0; {
-			var item string
-			item, todo = todo[0], todo[1:]
+		svc := SessionNew()
+		seen := map[string]bool{}
 
-			remotePager(svc, item, true, func(page *s3.ListObjectsV2Output) error {
-				// Anything that's a directory, traverse
-				for _, item := range page.CommonPrefixes {
-					uri := fmt.Sprintf("s3://%s/%s", *page.Name, *item.Prefix)
+		for _, sourceFile := range source {
+			for todo := []string{sourceFile}; len(todo) != 0; {
+				var item string
+				item, todo = todo[0], todo[1:]
 
-					todo = append(todo, uri)
-				}
-				if page.Contents != nil {
-					for _, item := range page.Contents {
-						// If we've traversed this path, don't od it twice
-						path := fmt.Sprintf("s3://%s/%s", *page.Name, *item.Key)
-						if seen[path] {
-							continue
-						}
-						seen[path] = true
+				remotePager(svc, item, true, func(page *s3.ListObjectsV2Output) error {
+					// Anything that's a directory, traverse
+					for _, item := range page.CommonPrefixes {
+						uri := fmt.Sprintf("s3://%s/%s", *page.Name, *item.Prefix)
 
-						queue <- NameQueue{bucket: *page.Name, key: *item.Key}
+						todo = append(todo, uri)
 					}
-				}
-				return nil
-			})
-		}
-	}
+					if page.Contents != nil {
+						for _, item := range page.Contents {
+							// If we've traversed this path, don't od it twice
+							path := fmt.Sprintf("s3://%s/%s", *page.Name, *item.Key)
+							if seen[path] {
+								continue
+							}
+							seen[path] = true
 
-	return nil
+							queue <- nameQueue{bucket: *page.Name, key: *item.Key}
+						}
+					}
+					return nil
+				})
+			}
+		}
+	}()
+
+	return queue
 }
 
-func downloadFiles(namesQueue chan NameQueue, dataQueue chan DataQueue) error {
-	defer close(dataQueue)
+func downloadFiles(namesQueue chan nameQueue) chan dataQueue {
+	queue := make(chan dataQueue)
 	waiter := sync.WaitGroup{}
 
-	svc := SessionNew()
-	downloader := s3manager.NewDownloaderWithClient(svc)
+	go func() {
+		defer close(queue)
 
-	for elem := range namesQueue {
-		bucket := elem.bucket
-		key := elem.key
-		waiter.Add(1)
+		svc := SessionNew()
+		downloader := s3manager.NewDownloaderWithClient(svc)
 
-		go func() {
-			defer waiter.Done()
+		for elem := range namesQueue {
+			bucket := elem.bucket
+			key := elem.key
+			waiter.Add(1)
 
-			buffer := aws.NewWriteAtBuffer([]byte{})
-			params := &s3.GetObjectInput{
-				Bucket: &bucket,
-				Key:    &key,
-			}
+			go func() {
+				defer waiter.Done()
 
-			_, err := downloader.Download(buffer, params)
-			if err != nil {
-				log.Fatal(err)
-			}
+				buffer := aws.NewWriteAtBuffer([]byte{})
+				params := &s3.GetObjectInput{
+					Bucket: &bucket,
+					Key:    &key,
+				}
 
-			dataQueue <- DataQueue{name: key, data: buffer.Bytes()}
-		}()
-	}
+				_, err := downloader.Download(buffer, params)
+				if err != nil {
+					log.Fatal(err)
+				}
 
-	waiter.Wait()
+				queue <- dataQueue{name: key, data: buffer.Bytes()}
+			}()
+		}
 
-	return nil
+		waiter.Wait()
+	}()
+
+	return queue
 }
 
-func addToZip(queue chan DataQueue, zipper ZipWriter) error {
-	defer zipper.Close()
+func addToZip(queue chan dataQueue) io.ReadCloser {
+	reader, writer := io.Pipe()
 
-	for elem := range queue {
-		if err := zipper.AddFile(elem.name, elem.data); err != nil {
-			return err
+	go func() {
+		zipper := NewZipper(writer)
+
+		defer func() {
+			zipper.Close()
+			writer.Close()
+		}()
+
+		for elem := range queue {
+			if err := zipper.AddFile(elem.name, elem.data); err != nil {
+				log.Fatal(err)
+			}
 		}
-	}
+	}()
 
-	return nil
+	return reader
 }
